ca96653370bec3b37e1df68986a0db465721a90f
gustavo panizzo <gfa@zumbi.com.ar>
2026-03-01 09:31:38 +0000

    remove date and time from messages since i get them from systemd anyway

--- src/main.rs
+++ src/main.rs
@@ -10,25 +10,21 @@ use surge_ping::{Client, Config, PingIdentifier, PingSequence, ICMP};
 
 fn main() {
     let rt = Runtime::new().unwrap();
-    let current_datetime = chrono::offset::Local::now();
-    println!("starting {}", current_datetime);
+    println!("starting");
 
     loop {
         {
             if !rt.block_on(pingfunc()) {
-                let current_datetime = chrono::offset::Local::now();
 
                 match rt.block_on(change_state(false)) {
-                    Ok(_) => println!("{} off", current_datetime),
-                    Err(e) => eprintln!("Error controlling plug: {} at {}", e, current_datetime),
+                    Ok(_) => println!("off"),
+                    Err(e) => eprintln!("Error controlling plug: {}", e),
                 }
                 thread::sleep(Duration::from_secs(20));
 
-                let current_datetime = chrono::offset::Local::now();
-
                 match rt.block_on(change_state(true)) {
-                    Ok(_) => println!("{} on", current_datetime),
-                    Err(e) => eprintln!("Error controlling plug: {} at {}", e, current_datetime),
+                    Ok(_) => println!("on"),
+                    Err(e) => eprintln!("Error controlling plug: {}", e),
                 }
                 thread::sleep(Duration::from_secs(600));
             }
@@ -40,8 +36,7 @@ fn main() {
 async fn change_state(state: bool) -> Result<(), Box<dyn Error>> {
     let device = ApiClient::new("foo", "bar").p115("192.168.94.70").await?;
 
-    let current_datetime = chrono::offset::Local::now();
-    println!("turning the device: {} at {}", state, current_datetime);
+    println!("turning the device: {}", state);
 
     if state {
         device.on().await?;
